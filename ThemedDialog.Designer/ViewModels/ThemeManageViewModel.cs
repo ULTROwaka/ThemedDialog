e4e68250b1282bbf0fa1c27faafa8115592ec3dc
Никита <click.95@mail.ru>
2020-01-31 00:40:24 +0700

    Kostiled live search + edit

--- ThemedDialog.Designer/ViewModels/ThemeManageViewModel.cs
+++ ThemedDialog.Designer/ViewModels/ThemeManageViewModel.cs
@@ -1,21 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Reactive.Linq;
 using DynamicData;
-using DynamicData.Binding;
 using ReactiveUI;
 using ReactiveUI.Fody.Helpers;
 using ThemedDialog.Core;
-using Windows.UI.Xaml;
+
 
 namespace ThemedDialog.Designer.ViewModels
 {
     public class ThemeManageViewModel : ReactiveObject
     {
-        private readonly SourceList<Theme> Themes;
+        public List<DialogCharacter> Characters { get; set; }
+        public List<Theme> Themes { get; set; }
         public List<Theme> SearchResults { [ObservableAsProperty] get; }
         [Reactive]
         public Theme SelectedTheme { get; set; }
@@ -30,14 +29,9 @@ namespace ThemedDialog.Designer.ViewModels
 
         public ThemeManageViewModel(IEnumerable<DialogCharacter> characters, IEnumerable<Theme> themes)
         {
-            SearchResults = new List<Theme>();
-            Themes = new SourceList<Theme>();
-            /*Themes.Connect()
-                  .Filter(t => t.Name.ToUpper().Contains(SearchTerm?.ToUpper() ?? string.Empty))                
-                  .ObserveOnDispatcher()
-                  .Bind(SearchResults)
-                  .Subscribe();*/
-            Themes.AddRange(themes);
+            Themes = new List<Theme>(themes);
+            Characters = new List<DialogCharacter>(characters);
+            SearchResults = new List<Theme>(Themes);
 
             this.WhenAnyValue(x => x.SearchTerm)
                 .Throttle(TimeSpan.FromMilliseconds(300))
@@ -48,28 +42,33 @@ namespace ThemedDialog.Designer.ViewModels
                 .ToPropertyEx(this, x => x.SearchResults);
 
             this.WhenAnyValue(x => x.SelectedTheme)
-                .Where(theme => theme != null)
-                .Select(theme => theme.Name)
+                .Where(selected => selected != null)
+                .Select(selected => selected.Name)
                 .Subscribe(name => EditThemeName = name);
 
             this.WhenAnyValue(x => x.SelectedTheme)
-                .Select(theme => theme != null)
-                .ToPropertyEx(this, x => x.CanEdit);             
+                .Select(x => x != null)
+                .ToPropertyEx(this, x => x.CanEdit);
 
             this.WhenAnyValue(x => x.SelectedTheme)
-                .Select(theme => theme != null)
+                .Select(x => x != null)
                 .ToPropertyEx(this, x => x.CanDelete);
         }
-
         private IEnumerable<Theme> Search(string term)
         {
-            var result = SearchResults.Where(t => t.Name.ToUpper().Contains(term?.ToUpper() ?? string.Empty));
-            return result;
+            if (term == null)
+            {
+                term = string.Empty;
+            }
+            var themes = Themes.Where(t => t.Name.ToUpper().Contains(term.ToUpper()));
+            return themes;
         }
 
         public void Edit()
-        {         
-            Themes.Replace(SelectedTheme, new Theme(EditThemeName));
+        {
+            var theme = Themes.Where(t => t.Name.Equals(SelectedTheme.Name)).Single();
+            theme.Name = EditThemeName;
+            SearchTerm = SearchTerm + string.Empty;
         }
     }
 }
